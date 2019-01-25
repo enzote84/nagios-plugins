78cca4dde22de79d208d16acf77f76e09af598d6
Enzo Medina <davidenzomedina@gmail.com>
2019-01-25 10:52:08 -0300

    Version 1.3. Implement minor changes suggested by community.

--- check_ibm_v7000_perf.sh
+++ check_ibm_v7000_perf.sh
@@ -17,9 +17,24 @@
 #     QUERY=`ssh -i $SSHKEY nagios@$HOSTNAME ...
 #   Use nagios's private key when invoking this script.
 #
+# SETUP:
+# 1.- Login to Nagios Server with nagios user. 
+# 2.- Generate a SSH key. 
+# 3.- Login to v7000, create a nagios user and attach the public SSH key to it. 
+# 4.- Copy the plugin to Nagios Server: /usr/local/nagios/libexec/check_ibm_v7000_perf.sh 
+# 5.- Test it: 
+#   /usr/local/nagios/libexec/check_ibm_v7000_perf.sh -H 192.168.1.100 -K /home/nagios/.ssh/id_rsa 
+# 6.- Create a command in nagios: 
+#   define command { 
+#     command_name check_v7000_performance 
+#     command_line $USER1$/check_ibm_v7000_perf.sh -H $HOSTADDRESS$ -K /home/nagios/.ssh/id_rsa 
+#   }
+#
 # COMMENTS ON VERSIONS:
 # 1.0 Base
 # 1.1 Changed single [] for [[]] (Thanks Andrew Haji)
+# 1.2 Added unknown result if query fails (Thanks carlinhos700)
+# 1.3 Spaces added in performance metrics (Thanks npx)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -34,7 +49,7 @@ STATE_UNKNOWN=3
 # Plugin info
 #
 AUTHOR="DEMR"
-VERSION="1.1"
+VERSION="1.3"
 PROGNAME=$(basename $0)
 
 print_version() {
@@ -129,6 +144,9 @@ if [[ $CPU_PC -gt $CCPU ]] || [[ $COMPRESSION_CPU_PC -gt $CCOMP ]] || [[ $WRITE_
 elif [[ $CPU_PC -gt $WCPU ]] || [[ $COMPRESSION_CPU_PC -gt $WCOMP ]] || [[ $WRITE_CACHE_PC -gt $WWCAC ]] || [[ $TOTAL_CACHE_PC -gt $WTCAC ]] || [[ $BIGGEST_LATENCY -gt $WLAT ]]; then
 	FINAL_STATUS="WARNING"
 	RETURN_STATUS=$STATE_WARNING
+elif [[ -z $CPU_PC ]] || [[ -z $COMPRESSION_CPU_PC ]] || [[ -z $WRITE_CACHE_PC ]] || [[ -z $TOTAL_CACHE_PC ]] || [[ -z $BIGGEST_LATENCY ]]; then
+	FINAL_STATUS="UNKNOWN"
+	RETURN_STATUS=$STATE_UNKNOWN
 else
 	FINAL_STATUS="OK"
 	RETURN_STATUS=$STATE_OK
@@ -136,8 +154,10 @@ fi
 # Add metrics:
 FINAL_STATUS="$FINAL_STATUS - CPU: $CPU_PC%, Compression CPU: $COMPRESSION_CPU_PC%, Write Cache: $WRITE_CACHE_PC%, Total Cache: $TOTAL_CACHE_PC%, Latency: $BIGGEST_LATENCY ms"
 # Performance string:
-PERFORMANCE="cpu=$CPU_PC;comp_cpu=$COMPRESSION_CPU_PC;wcache=$WRITE_CACHE_PC;tcache=$TOTAL_CACHE_PC;latency=$BIGGEST_LATENCY"
-PERFORMANCE="$PERFORMANCE;fc_mb=$FC_MB;fc_io=$FC_IO;sas_mb=$SAS_MB;sas_io=$SAS_IO;iscsi_mb=$ISCSI_MB;iscsi_io=$ISCSI_IO"
+#PERFORMANCE="cpu=$CPU_PC;comp_cpu=$COMPRESSION_CPU_PC;wcache=$WRITE_CACHE_PC;tcache=$TOTAL_CACHE_PC;latency=$BIGGEST_LATENCY"
+PERFORMANCE="cpu=$CPU_PC; comp_cpu=$COMPRESSION_CPU_PC; wcache=$WRITE_CACHE_PC; tcache=$TOTAL_CACHE_PC; latency=$BIGGEST_LATENCY"
+#PERFORMANCE="$PERFORMANCE;fc_mb=$FC_MB;fc_io=$FC_IO;sas_mb=$SAS_MB;sas_io=$SAS_IO;iscsi_mb=$ISCSI_MB;iscsi_io=$ISCSI_IO"
+PERFORMANCE="$PERFORMANCE; fc_mb=$FC_MB; fc_io=$FC_IO; sas_mb=$SAS_MB; sas_io=$SAS_IO; iscsi_mb=$ISCSI_MB; iscsi_io=$ISCSI_IO"
 FINAL_STATUS="$FINAL_STATUS|$PERFORMANCE"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
